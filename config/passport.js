cef7ea487dca5f8ef6d2b7ab60a79347409ba5a0
abdulshakur <abdulshakur486@gmail.com>
2021-11-25 20:52:36 +0300

    fixed redirect path

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ passport.use(
   new GoogleStrategy(
     {
       callbackURL:
-        " https://audiophile-e-commerce.herokuapp.com/auth/google/redirect",
+        "https://audiophile-e-commerce.herokuapp.com/auth/google/redirect",
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SERCRET,
     },
